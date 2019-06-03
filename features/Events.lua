fcfcf25cae511101e407c0c83f46dc644d17a5d7
Soleo <soleoofficial@gmail.com>
2019-06-03 19:41:12 +0200

    Update Events.lua

--- features/Events.lua
+++ features/Events.lua
@@ -2,23 +2,20 @@ local chatGuildEvent = CreateFrame("Frame")
 chatGuildEvent:RegisterEvent("CHAT_MSG_GUILD") -- CHAT_MSG_SAY
 chatGuildEvent:SetScript("OnEvent", function(self, event, message, sender, ...)
 
-	if string.find(message, sender .. " souhaite aussi participer à l'event !") then -- Pour les joueurs en retard
-		if getArrayIndex(playingUsers, sender) ~= nil then
-			SendChatMessage("Nope !", "WHISPER", nil, sender)
+	if string.find(message, sender .. " souhaite aussi participer à l'event !") or string.find(message, sender .. " participe à l'event !") then -- Pour les joueurs en retard
+		if vAGet("isStarted") == true then
+			if getArrayIndex(playingUsers, sender) ~= nil then
+				SendChatMessage("Nope !", "WHISPER", nil, sender)
+			else
+				SendChatMessage("Clé d'évènement : " .. vAGet("key"), "WHISPER", nil, sender) -- SAY
+				SendChatMessage("Le Maître du Jeu sera : " .. "Soleo", "WHISPER")
+				-- SendChatMessage("Date de fin maximale : " .. day .. "/06/2019 " .. endHour .. "h" .. minutes, "WHISPER")
+				-- SendChatMessage(vAGet("key"), "WHISPER", nil, sender)
+				playingUsers[table.getn(playingUsers) + 1] = sender
+				-- MP -> CLE DEVENEMENT + DATE FIN + 
+			end
 		else
-			SendChatMessage("Clé d'évènement : " .. vAGet("key"), "WHISPER", nil, sender) -- SAY
-			-- SendChatMessage("Le Maître du Jeu sera : " .. "Soleo", "WHISPER")
-			-- SendChatMessage("Date de fin maximale : " .. day .. "/06/2019 " .. endHour .. "h" .. minutes, "WHISPER")
-			-- SendChatMessage(vAGet("key"), "WHISPER", nil, sender)
-			playingUsers[table.getn(playingUsers) + 1] = sender
-			-- MP -> CLE DEVENEMENT + DATE FIN + 
-		end
-
-	elseif string.find(message, sender .. " participe à l'event !") then -- Pour les joueurs à l'heure
-		if getArrayIndex(playingUsers, sender) ~= nil then
-			SendChatMessage("Nope !", "WHISPER", nil, sender)
-		else
-			playingUsers[table.getn(playingUsers) + 1] = sender
+			SendChatMessage("Aucun event n'est en cours !", "WHISPER", nil, sender)
 		end
 	end
 end)
